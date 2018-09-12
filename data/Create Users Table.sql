a8f32062b02bf5cf2c730e30d155f18289ec6b46
IcyWillow <vinilodeon@hotmail.com>
2018-09-12 15:06:54 +0200

    Upload (not working yet)
    Some changes for edit

--- data/Create Users Table.sql
+++ data/Create Users Table.sql
@@ -10,3 +10,12 @@ USE gamingportal;
     password VARCHAR(255) NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
+
+
+
+CREATE TABLE game (
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name NVARCHAR(50) NOT NULL,
+    description nvarchar(500) NOT NULL,
+    picture_directory nvarchar(100));
+    
--- view/edit.php
+++ view/edit.php
@@ -9,22 +9,22 @@
 </head>
 
 <body>
-  <form action="../public/index.php" method="post" class="edit-form">
-    <h1>Create / Edit</h1>
-    <label for="game-title">Title:</label>
-    <input type="text" name="game-title" id="game-title" required>
-    <label for="game-publisher">Publisher:</label>
-    <input type="text" name="game-publisher" id="game-publisher" required>
-    <label for="game-description">Description:</label>
-    <input type="text" name="game-description" id="game-description" required>
-    <label for="game-image">Picture:</label>
-    <input type="file" src="game-image" alt="game-image">
+    <form action="../lib/upload.php" method="post" class="edit-form" enctype="multipart/form-data">
+        <h1>Create / Edit</h1>
+        <label for="game-title">Title:</label>
+        <input type="text" name="game-title" id="game-title" required />
+        <label for="game-publisher">Publisher:</label>
+        <input type="text" name="game-publisher" id="game-publisher" required />
+        <label for="game-description">Description:</label>
+        <input type="text" name="game-description" id="game-description" required />
+        <label for="game-image">Picture:</label>
+        <input type="file" name="fileToUpload" id="fileToUpload"    alt="game-image" required/>
 
-    <div class="button-container">
-      <button class="cancel-button" type="reset">Cancel</button>
-      <button class="submit-button" type="submit">Submit</button>
-    </div>
-  </form>
+        <div class="button-container">
+            <button class="cancel-button" type="reset">Cancel</button>
+            <button class="submit-button" type="submit">Submit</button>
+        </div>
+    </form>
 </body>
 
 </html>
