1ab0d4be4b033fb33b592f2e9e50dd7b53f4a688
Unknown <selim.swisscom@gmail.com>
2018-09-12 14:54:20 +0200

    THE STYLE

--- view/login.php
+++ view/login.php
@@ -94,11 +94,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <meta charset="UTF-8" />
     <title>Login</title>
     <link rel="stylesheet" type="text/css" href="../public/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans" rel="stylesheet">
 </head>
 <body>
     <div class="wrapper">
-        <h2>Login</h2>
-        <p>Please fill in your credentials to login.</p>
+        <h2 id="loginTitle">Login</h2>
+        <p class="introText">Please fill in your credentials to login.</p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
                 <label>Username</label>
@@ -117,7 +118,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Login" />
             </div>
-            <p>
+            <p class="introText">
                 Don't have an account?
                 <a href="./register.php">Sign up now</a>.
             </p>
