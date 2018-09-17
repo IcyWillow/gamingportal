36bae6afc9469ae1ffcbd73ed8eb776d148ef583
Unknown <selim.swisscom@gmail.com>
2018-09-17 13:38:54 +0200

    fixfix

--- controller/GameController.php
+++ controller/GameController.php
@@ -13,39 +13,23 @@ class Game {
         $this->imgSource = $imgSource;
     }
 
-
     //Get a game by ID
     public function getGameById($id){
-
         include("../config/config.php");
-
-
-
-
         $query= $link->query("SELECT * FROM game where id = " . $id . ";");
-
-
-
-
-
             while ($rows = $query->fetch_array(MYSQLI_ASSOC)) {
 
                 $this->gameName = $rows['name'];
                 $this->publisher = $rows['publisher'];
                 $this->description = $rows['description'];
                 $this->imgSource = $rows['picture_directory'];
-
-
                      }
-
     }
-
 
     //Get a game by name
     public function getGameByName($g_name){
 
         include("../config/config.php");
-
 
         $query= $link->query("SELECT * FROM game where name LIKE '%" . $g_name . "%';");
 
@@ -54,12 +38,7 @@ class Game {
             $foundGame = new Game($rows['name'], $rows['publisher'], $rows['description'], $rows['picture_directory']);
 
             $foundGame->makeCard();
-
-
         }
-
-
-
     }
 
     //Creates new Gamecard for game
